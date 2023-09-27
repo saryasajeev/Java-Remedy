cb37fc2b6755363bda11353b7be129cc50fb06b9
saryasajeev <145546323+saryasajeev@users.noreply.github.com>
2023-09-27 14:18:54 +0530

    Add files via upload

---
+++ Numberpositive.java
@@ -0,0 +1,28 @@
+/*******************************************
+ *File       :Percentage.java
+ *Description:Finding attendence mark
+ *author     :sarya sajeev
+ *version    :1.0
+ *Date       :26/09/2023
+*******************************************/
+import java.util.Scanner;
+public class Numberpositive {
+	public static void main(String[]args) {
+		int number;
+		Scanner Sc=new Scanner(System.in);
+		System.out.println("Input a number");
+		number=Sc.nextInt();
+		if(number>0) {
+			System.out.println("The number is positive");
+		}
+		else if(number<0) {
+			System.out.println("The number is negative");
+		}
+		else {
+			System.out.println("The number is zero");
+		}
+		
+			
+		}
+		
+	}
