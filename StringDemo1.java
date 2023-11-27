0a0070ec272048bd9452806c727973554b7502e5
saryasajeev <145546323+saryasajeev@users.noreply.github.com>
2023-11-27 09:39:30 +0530

    Create StringDemo.java

---
+++ StringDemo1.java
@@ -0,0 +1,12 @@
+public class StringDemo {
+	public static void main(String[] args) {
+		String str1;
+		str1="Java Exercises!";
+		System.out.println("Original String="+str1);
+		int a=str1.charAt(0);
+		int b=str1.charAt(10);
+		System.out.println("The character at position 0 is"+(char)a);
+		System.out.println("The character at position 10 is"+(char)b);
+	}
+
+}
